a58f834ba6c9f78b9db344e4bef0c2f0141c60b9
Arseny K <arsenykonohov@gmail.com>
2023-02-16 14:55:24 +0300

    get current user name LWC;

--- force-app/main/default/lwc/selector/selector.js
+++ force-app/main/default/lwc/selector/selector.js
@@ -1,7 +1,30 @@
-import { LightningElement } from "lwc";
+import { LightningElement, wire } from "lwc";
+import { getRecord } from "lightning/uiRecordApi";
+import USER_ID from "@salesforce/user/Id";
+import USER_NAME from "@salesforce/schema/User.Name";
+const fields = [USER_ID, USER_NAME];
 
 export default class Selector extends LightningElement {
-    selectedProductId;
+    get userId() {
+        return USER_ID;
+    }
+
+    @wire(getRecord, { recordId: "$userId", fields })
+    _user = {};
+
+    get isUserFound() {
+        return this._user?.data?.fields;
+    }
+
+    get user() {
+        const value = this._user?.data.fields || {};
+        console.log('value:', value);
+        return value;
+    }
+
+    get name() {
+        return this.user?.Name?.value;
+    }
 
     handleProductSelected(evt) {
         this.selectedProductId = evt.detail;
