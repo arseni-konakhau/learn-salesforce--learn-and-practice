c9486ffd3bf21b030a613a5c4249fdb3b391ab36
Arseny Konakhau <arseni.konakhau@gmail.com>
2023-02-26 23:05:52 +0300

    fixed config resolutions for test run representation;

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,9 @@ const { jestConfig } = require("@salesforce/sfdx-lwc-jest/config");
 module.exports = {
     ...jestConfig,
     moduleNameMapper: {
-        "^lightning/button$": "<rootDir>/force-app/test/jest-mocks/lightning/button",
-        "^thunder/hammerButton$": "<rootDir>/force-app/test/jest-mocks/thunder/hammerButton"
+        "^lightning/button$": "<rootDir>/force-app/test/jest-mocks/lightning/button/button",
+        "^thunder/hammerButton$": "<rootDir>/force-app/test/jest-mocks/thunder/hammerButton/hammerButton"
     },
+    resolver: "<rootDir>/node_modules/@salesforce/sfdx-lwc-jest/src/resolver.js",
     modulePathIgnorePatterns: ["<rootDir>/.localdevserver"]
 };
